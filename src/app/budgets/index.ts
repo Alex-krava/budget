57a2a83c54ffa421eb567c765b529194b589dd20
Alexandr Kravchenko <alex.krava@live.com>
2019-06-22 18:04:26 +0300

    Auto-generated commit

--- src/app/budgets/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './budgets.component';
-export * from './create-budget/create-budget.component';
-export * from './budget-details/budget-details.component';
-export * from './add-transaction/add-transaction.component';
